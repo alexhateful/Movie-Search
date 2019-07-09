8cd91e778538ee4d17bfce5ec50a49594ff77f23
alexhateful <alexhateful@gmail.com>
2019-07-10 00:54:44 +0300

    Updates

---
+++ precache-manifest.bf2fadce29d619c2876801f4a4a14709.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a2c5699915fc01e93e484717977e2c",
+    "url": "/Movie-Search/index.html"
+  },
+  {
+    "revision": "8a4419737182f460720d",
+    "url": "/Movie-Search/static/css/2.509658c0.chunk.css"
+  },
+  {
+    "revision": "6a5b36d13d218fb71481",
+    "url": "/Movie-Search/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "8a4419737182f460720d",
+    "url": "/Movie-Search/static/js/2.d3006baa.chunk.js"
+  },
+  {
+    "revision": "6a5b36d13d218fb71481",
+    "url": "/Movie-Search/static/js/main.172d4b7b.chunk.js"
+  },
+  {
+    "revision": "5d6291419a1179634611",
+    "url": "/Movie-Search/static/js/runtime~main.2f6b2dbd.js"
+  },
+  {
+    "revision": "261f22ef50f18c010da606027aee091c",
+    "url": "/Movie-Search/static/media/star_grey.261f22ef.png"
+  },
+  {
+    "revision": "1c7624aa5d277d6d57a034a82d78a8d3",
+    "url": "/Movie-Search/static/media/star_yellow.1c7624aa.png"
+  }
+]);
---
+++ precache-manifest.fac5668bafd02cb34ce5ab1fe3c7f560.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e555568d5ee8fd9141bef02fb944eee5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9c5d626d7ee34cc5618",
+    "url": "/static/css/2.509658c0.chunk.css"
+  },
+  {
+    "revision": "12150cbd510c7777b3fe",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "e9c5d626d7ee34cc5618",
+    "url": "/static/js/2.d5d16292.chunk.js"
+  },
+  {
+    "revision": "12150cbd510c7777b3fe",
+    "url": "/static/js/main.08cab1e9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "261f22ef50f18c010da606027aee091c",
+    "url": "/Movie-Search/static/media/star_grey.261f22ef.png"
+  },
+  {
+    "revision": "1c7624aa5d277d6d57a034a82d78a8d3",
+    "url": "/Movie-Search/static/media/star_yellow.1c7624aa.png"
+  }
+]);
